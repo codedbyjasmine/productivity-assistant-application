06383182fb47683e73f2628b3bbde9858b2e2d87
Estevivanco <esteban@vivanco.se>
2025-12-19 10:50:56 +0100

    Moved components to homepage + css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Header from "../components/header/Header";
 import { AuthContext } from "../context/Context";
-
+import s from './Homepage.module.css'
+import Overview from "../components/overview/Overview";
+import Todos from "../components/todos-and-activities/Todos/Todos";
+import Habits from "../components/habits/Habits/Habits";
+import EventPlanner from "../components/event-planner/EventPlanner";
+import '../App.css'
+useState
 
 const HomePage = () => {
-    const {handleLogout} = useContext(AuthContext)
+    const {handleLogout,mode,currentUser} = useContext(AuthContext)
 
     return(
-        <>
-        <h1>Productivity Assistant</h1>
-        <button onClick={handleLogout}>Log Out</button>
-        <Header/>
-        </>
+        <div className={s.body}>
+            <div className={s.titleWrapper}>
+                <p><strong>Productivity Assistant</strong></p>
+                <button onClick={handleLogout}>Log Out</button>
+            </div>
+            <div className={s.bodyContainer}>
+                <Header/>
+                {mode === "overview" && <Overview/> }
+                {mode === "todos" && <Todos/> }
+                {mode === "habits" && <Habits/> }
+                {mode === "eventplanner" && <EventPlanner/> }
+            </div>
+        </div>
     )
 }
 
