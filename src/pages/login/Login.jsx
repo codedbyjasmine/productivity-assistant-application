3ae45d869ffc5dce2a45bc21693ab7f1a224f325
Estevivanco <esteban@vivanco.se>
2025-12-19 10:49:51 +0100

    Fixed css and spelling

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -22,60 +22,61 @@ const Login = () => {
     }, [])
     
     return (
-        <div className={s.userContainer}>
-            <div className={s.welcomeContainer}>
-                <h2>Hi, welcome back!</h2>
-                <p>Please log in with the username and password you created before</p>
-            </div>
-            <form onSubmit={onSubmit} className={s.formContainer}>
-                <div className={s.inputContainer}>
-                    <label htmlFor="username">Username</label>
-                    <input
-                    className={usernameErr ? s.inputError : s.input}
-                    type="text"
-                    id="usernameId"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e)=> {
-                        setUsernameErr(false)
-                        setUserErr(false)
-                        setUsername(e.target.value)
-                    }} />
+        <div className={s.body}>
+            <div className={s.userContainer}>
+                <div className={s.welcomeContainer}>
+                    <h2>Hi, welcome back!</h2>
+                    <p>Please log in with the username and password you created before</p>
                 </div>
-                {usernameErr && 
-                <div className={s.errorMessage}>
-                    <p>❗Username is required</p>
-                </div>}
-
-                <div className={s.inputContainer}>
-                    <label htmlFor="password">Password</label>
-                    <input
-                    className={passwordErr ? s.inputError : s.input}
-                    type="text"
-                    id="passwordId"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e)=>{
-                        setPasswordErr(false)
-                        setUserErr(false)
-                        setPassword(e.target.value)}} />
-                </div>
-                {passwordErr && 
-                <div className={s.errorMessage}>
-                    <p>❗Password is required</p>
-                </div>}
-                {userErr && 
-                <div className={s.errorMessage}>
-                    <p>❗User not found</p>
-                </div>}
-                <div className={s.navigateContainer}>
-                    <button type="submit">Login now</button>
-                    <div>
-                        <p>Don't have an account? <Link to="register">Create account</Link></p>
+                <form onSubmit={onSubmit} className={s.formContainer}>
+                    <div className={s.inputContainer}>
+                        <label htmlFor="username">Username</label>
+                        <input
+                        className={usernameErr ? s.inputError : s.input}
+                        type="text"
+                        id="usernameId"
+                        placeholder="Enter username"
+                        value={username}
+                        onChange={(e)=> {
+                            setUsernameErr(false)
+                            setUserErr(false)
+                            setUsername(e.target.value)
+                        }} />
                     </div>
-                </div>
-                
-            </form>
+                    {usernameErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗Username is required</p>
+                    </div>}
+                    <div className={s.inputContainer}>
+                        <label htmlFor="password">Password</label>
+                        <input
+                        className={passwordErr ? s.inputError : s.input}
+                        type="text"
+                        id="passwordId"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e)=>{
+                            setPasswordErr(false)
+                            setUserErr(false)
+                            setPassword(e.target.value)}} />
+                    </div>
+                    {passwordErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗Password is required</p>
+                    </div>}
+                    {userErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗User not found</p>
+                    </div>}
+                    <div className={s.navigateContainer}>
+                        <button type="submit">Login now</button>
+                        <div>
+                            <p>Don't have an account? <Link to="register">Create account</Link></p>
+                        </div>
+                    </div>
+            
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/login/Register.jsx
+++ src/pages/login/Register.jsx
@@ -26,74 +26,74 @@ const Register = () => {
 
     }
     return (
-        <div className={s.userContainer}>
-            <div className={s.welcomeContainer}>
-                <h2>Get started now!</h2>
-                <p>Register with your username and create your account!</p>
-            </div>
-            <form onSubmit={onSubmit} className={s.formContainer}>
-                <div className={s.inputContainer}>
-                    <label htmlFor="username">Username</label>
-                    <input
-                    className={usernameErr ? s.inputError : s.input}
-                    type="text"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e)=>{
-                        setUsernameErr(false)
-                        setUserErr(false)
-                        setUsername(e.target.value)}} />
+        <div className={s.body}>
+            <div className={s.userContainer}>
+                <div className={s.welcomeContainer}>
+                    <h2>Get started now!</h2>
+                    <p>Register with your username and create your account!</p>
                 </div>
-                {usernameErr && 
-                <div className={s.errorMessage}>
-                    <p>❗Usernamw is required</p>
-                </div>}
-
-                <div className={s.inputContainer}>
-                    <label htmlFor="password">Password</label>
-                    <input
-                    className={passwordErr ? s.inputError : s.input} 
-                    type="text"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e)=>{
-                        setPasswordErr(false)
-                        setUserErr(false)
-                        setPassword(e.target.value)}} />
-                </div>
-                {passwordErr && 
-                <div className={s.errorMessage}>
-                    <p>❗Password is required</p>
-                </div>}
-
-                <div className={s.inputContainer}>
-                    <label htmlFor="password">Confirm password</label>
-                    <input
-                    className={confirmErr ? s.inputError : s.input} 
-                    type="text"
-                    placeholder="Enter password"
-                    value={confirmP}
-                    onChange={(e)=>{
-                        setConfirmErr(false)
-                        setUserErr(false)
-                        setConfirmP(e.target.value)}} />
-                </div>
-                {confirmErr && 
-                <div className={s.errorMessage}>
-                    <p>❗Confirm your password</p>
-                </div>}
-                {userErr && 
-                <div className={s.errorMessage}>
-                    <p>❗Username already exists</p>
-                </div>}
-                
-                <div className={s.navigateContainer}>
-                    <button type="submit">Create</button>
-                     <div>
-                        <p>Already have an account? <Link to="/">Login</Link></p>
+                <form onSubmit={onSubmit} className={s.formContainer}>
+                    <div className={s.inputContainer}>
+                        <label htmlFor="username">Username</label>
+                        <input
+                        className={usernameErr ? s.inputError : s.input}
+                        type="text"
+                        placeholder="Enter username"
+                        value={username}
+                        onChange={(e)=>{
+                            setUsernameErr(false)
+                            setUserErr(false)
+                            setUsername(e.target.value)}} />
                     </div>
-                </div>
-            </form>
+                    {usernameErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗Username is required</p>
+                    </div>}
+                    <div className={s.inputContainer}>
+                        <label htmlFor="password">Password</label>
+                        <input
+                        className={passwordErr ? s.inputError : s.input}
+                        type="text"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e)=>{
+                            setPasswordErr(false)
+                            setUserErr(false)
+                            setPassword(e.target.value)}} />
+                    </div>
+                    {passwordErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗Password is required</p>
+                    </div>}
+                    <div className={s.inputContainer}>
+                        <label htmlFor="password">Confirm password</label>
+                        <input
+                        className={confirmErr ? s.inputError : s.input}
+                        type="text"
+                        placeholder="Enter password"
+                        value={confirmP}
+                        onChange={(e)=>{
+                            setConfirmErr(false)
+                            setUserErr(false)
+                            setConfirmP(e.target.value)}} />
+                    </div>
+                    {confirmErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗Invalid or missing input</p>
+                    </div>}
+                    {userErr &&
+                    <div className={s.errorMessage}>
+                        <p>❗Username already exists</p>
+                    </div>}
+            
+                    <div className={s.navigateContainer}>
+                        <button type="submit">Create</button>
+                         <div>
+                            <p>Already have an account? <Link to="/">Login</Link></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
